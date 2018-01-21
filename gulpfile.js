f81821e301c0996ba19eee2123b365a843bac876
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-01-20 21:01:11 -0500

    Remove CJS (CommonJS) build since UMD (Universal Module Definition) works in CJS environments

--- gulpfile.js
+++ gulpfile.js
@@ -12,14 +12,7 @@ const browserSync = require('browser-sync').create()
 const pack = require('./package.json')
 const packageRollup = require('./utils/package-rollup')
 
-gulp.task('compress', ['js-lint', 'commonjs', 'dev', 'production', 'all', 'all.min'])
-
-gulp.task('commonjs', () => {
-  return packageRollup({
-    dest: 'dist/' + pack.name + '.common.js',
-    format: 'cjs'
-  })
-})
+gulp.task('compress', ['js-lint', 'dev', 'production', 'all', 'all.min'])
 
 gulp.task('dev', () => {
   return packageRollup({
